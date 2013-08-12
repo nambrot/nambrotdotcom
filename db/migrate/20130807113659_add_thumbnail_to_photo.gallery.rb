cccd0f28a44a5713e1185869f79d2843f9039d3a
Nam Chu Hoai <nambrot@googlemail.com>
2013-08-12 16:13:27 +0200

    fix migration

--- db/migrate/20130807113659_add_thumbnail_to_photo.gallery.rb
+++ db/migrate/20130807113659_add_thumbnail_to_photo.gallery.rb
@@ -2,6 +2,6 @@
 class AddThumbnailToPhoto < ActiveRecord::Migration
   def change
     add_column :gallery_photos, :thumbnail, :string
-    remove_columns :gallery_photos, :images, :text
+    remove_columns :gallery_photos, :images
   end
 end
